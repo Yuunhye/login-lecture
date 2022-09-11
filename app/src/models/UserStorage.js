466804d22d77a031868235ba3fcd1e50870d4336
Yuunhye <dbsgpp1256@naver.com>
2022-09-12 00:01:30 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,15 +1,23 @@
 "use strict";
 
+const fs = require("fs").promises;
+
 //파일명과 같은 이름으로 해주는게 좋음
 class UserStorage {
-    static #users = {
-        id : ["dbsgpp", "yuunhye", "orange"],
-        password : ["1234", "5678", "123456"],
-        name : ["박윤혜", "김김김", "이이이"]
-    };
+    
+    static #getUserInfo(data,id) {     //은닉화된 method
+        const users = JSON.parse(data);
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);   //users의 key값들로 배열을 만듦. => [id, password, name]
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+        return userInfo;
+    }
 
     static getUsers(...fields) {
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
             if(users.hasOwnProperty(field)){    //users에 field에 해당하는 key값이 존재하냐
                 newUsers[field] = users[field];
@@ -20,19 +28,15 @@ class UserStorage {
     }
 
     static getUserInfo(id) {        //id를 넣어주면 그 id에 해당하는 password, name까지 포함한 object를 반환하는 method.
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users);   //users의 key값들로 배열을 만듦. => [id, password, name]
-        const userInfo = usersKeys.reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
-
-        return userInfo;
+        return fs.readFile("./src/databases/users.json")
+        .then((data) => {   //readFile이 성공적으로 수행되면 실행
+            return this.#getUserInfo(data,id)
+        })
+        .catch(console.error);     // (err) => console.error(err)
     }
 
     static save(userInfo) {
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.password.push(userInfo.password);
